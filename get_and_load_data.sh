215c2c53d1014df777baa7a721e394420e709b30
pierre-lamarche <lamarche.p@gmail.com>
2020-04-21 17:18:33 +0200

    Spécifier le password

--- get_and_load_data.sh
+++ get_and_load_data.sh
@@ -2,11 +2,11 @@
 
 ## create schema rp2016
 
-psql -c 'CREATE SCHEMA rp2016'
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'CREATE SCHEMA rp2016'
 
 ## load logement table
 curl -o logement.zip https://www.insee.fr/fr/statistiques/fichier/4229099/RP2016_LOGEMT_csv.zip
-unzip logement.zip
-psql load_logement.sql
+unzip logement
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB load_logement.sql
 rm logement.zip
 rm FD_LOGEMT_2016.csv 
