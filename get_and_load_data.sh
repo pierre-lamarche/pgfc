7db3a1cd39124418f7f5f41d8bb8f9fd5d1fdbf0
pierre-lamarche <lamarche.p@gmail.com>
2020-04-21 19:40:41 +0200

    Ajout localhost sur psql

--- get_and_load_data.sh
+++ get_and_load_data.sh
@@ -2,11 +2,11 @@
 
 ## create schema rp2016
 
-PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'CREATE SCHEMA rp2016'
+psql -h localhost -p 5432 -c 'CREATE SCHEMA rp2016'
 
 ## load logement table
 curl -o logement.zip https://www.insee.fr/fr/statistiques/fichier/4229099/RP2016_LOGEMT_csv.zip
-unzip logement
+unzip logement.zip
 PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB load_logement.sql
 rm logement.zip
 rm FD_LOGEMT_2016.csv 
