db3723d9f7a4a65bfa60770c461aedb02d7bb298
pierre-lamarche <lamarche.p@gmail.com>
2020-04-23 20:36:59 +0200

    change quote

--- get_and_load_data.sh
+++ get_and_load_data.sh
@@ -29,6 +29,6 @@ unzip /tmp/logement.zip -d /tmp/
 read_meta rp2016 logement /tmp/varmod_LOGEMT_2016.csv /tmp/load_logement.sql
 
 PGPASSWORD=$POSTGRES_PASSWORD psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" /tmp/load_logement.sql
-PGPASSWORD=$POSTGRES_PASSWORD psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c 'COPY rp2016.logement FROM "/tmp/FD_LOGEMT_2016.csv" DELIMITER ";" CSV HEADER ;'
+PGPASSWORD=$POSTGRES_PASSWORD psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "COPY rp2016.logement FROM '/tmp/FD_LOGEMT_2016.csv' DELIMITER ';' CSV HEADER ;"
 rm /tmp/logement.zip
 rm /tmp/FD_LOGEMT_2016.csv 
