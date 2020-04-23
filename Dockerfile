4bb90c9f9d6a5c0a495739ee883fa8cd4cbc611b
pierre-lamarche <lamarche.p@gmail.com>
2020-04-23 19:27:30 +0200

    Retourne vers initdb

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ ENV annee=2016
 RUN apt-get update \
     && apt-get install -y zip curl postgresql-client-common postgresql-client
 
-COPY dl_donnees.sh /home/
-RUN /bin/bash -s 'chmod +x /home/dl_donnees.sh'
+#COPY dl_donnees.sh /home/
+#RUN /bin/bash -s 'chmod +x /home/dl_donnees.sh'
 COPY get_and_load_data.sh /docker-entrypoint-initdb.d/
-CMD ["/bin/sh", "/home/dl_donnees.sh"]
+#CMD ["/bin/sh", "/home/dl_donnees.sh"]
 #RUN /bin/bash -s 'chmod +x /home/get_and_load_data.sh'
 #CMD ["/bin/sh", "/home/get_and_load_data.sh"]
--- get_and_load_data.sh
+++ get_and_load_data.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
+## awk to generate sql script
+
+read_meta () {
+	nom_schema=$1
+	nom_table=$2
+	file_input=$3
+	file_output=$4 
+
+	awk -F ";" 'BEGIN{RS = "\r\n" 
+			print "CREATE TABLE '$nom_schema'.'$nom_table' \n("} 
+			NR > 1 { if ($1 != prev) {
+				if ($5 == "CHAR") print "\t" $1 " CHARACTER(" $6 ")"
+				if ($5 == "NUM") print "\t" $1 " NUMERIC"
+				}
+			prev = $1}
+			END {print ")"}' $file_input > $file_output
+}
+
 ## create schema rp2016
 
 PGPASSWORD=$POSTGRES_PASSWORD psql-U $POSTGRES_USER -d $POSTGRES_DB -c 'CREATE SCHEMA rp2016'
 
 ## load logement table
+
+curl -o logement.zip https://www.insee.fr/fr/statistiques/fichier/4229099/RP2016_LOGEMT_csv.zip
+unzip logement.zip -d /home/
+read_meta rp2016 logement /home/varmod_LOGEMT_2016.csv /home/load_logement.sql
 
 PGPASSWORD=$POSTGRES_PASSWORD psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" /home/load_logement.sql
 PGPASSWORD=$POSTGRES_PASSWORD psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c 'COPY rp2016.logement FROM "FD_LOGEMT_2016.csv" DELIMITER ";" CSV HEADER ;'
